1c268939740de64a960436c074b9f1723fcda1c9
Brian Azizi <briana@theodo.co.uk>
2021-02-13 13:46:56 +0000

    (red) add system test

---
+++ test/api.test.ts
@@ -0,0 +1,59 @@
+import { InMemoryDocumentClient } from "../src/documentClient";
+import { emptyTable } from "./utils";
+import axios from "axios";
+import * as uuid from "uuid";
+
+const url = "http:/localhost:3000";
+
+const addStock = (docClient: InMemoryDocumentClient) => async (
+  batches: [string, string, number, string][]
+) => {
+  const docClientRequests = batches.map(([ref, sku, qty, eta]) =>
+    docClient
+      .put({
+        TableName: "Allocations",
+        Item: {
+          PK: ref,
+          SK: eta,
+          Reference: ref,
+          Sku: sku,
+          PurchasedQuantity: qty,
+          Eta: eta,
+          Allocations: [],
+        },
+      })
+      .promise()
+  );
+
+  return await Promise.all(docClientRequests);
+};
+
+describe("Allocations API", () => {
+  let docClient: InMemoryDocumentClient;
+  beforeAll(() => {
+    docClient = new InMemoryDocumentClient();
+  });
+  beforeEach(async (done) => {
+    await emptyTable(docClient);
+    done();
+  });
+
+  it("returns allocations", async () => {
+    const [sku, otherSku] = [uuid.v4(), uuid.v4()];
+    const [earlyBatch, laterBatch, otherBatch] = [
+      uuid.v4(),
+      uuid.v4(),
+      uuid.v4(),
+    ];
+    await addStock(docClient)([
+      [laterBatch, sku, 100, "2011-01-02"],
+      [earlyBatch, sku, 100, "2011-01-01"],
+      [otherBatch, otherSku, 100, "NONE"],
+    ]);
+
+    const requestData = { orderId: uuid.v4(), sku, quantity: 3 };
+    const response = await axios.post(`${url}/allocate`, requestData);
+    expect(response.status).toBe(201);
+    expect(response.data).toBe(201);
+  });
+});

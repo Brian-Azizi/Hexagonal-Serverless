7d55e8eaf7247ef7ba11fac8760226f2e279ed0f
Brian Azizi <briana@theodo.co.uk>
2021-02-14 00:56:41 +0000

    Add more api tests

--- src/api.ts
+++ src/api.ts
@@ -6,6 +6,9 @@ import { DynamoDbRepository } from "./repository";
 const documentClient = new DynamoDbDocumentClient();
 const repository = new DynamoDbRepository(documentClient);
 
+const isValidSku = (sku: string, batches: model.Batch[]) =>
+  batches.map((b) => b.sku).includes(sku);
+
 export const allocate: Handler = async (event: APIGatewayProxyEvent) => {
   const requestData = JSON.parse(event.body || "");
   const batches = await repository.list();
@@ -14,7 +17,31 @@ export const allocate: Handler = async (event: APIGatewayProxyEvent) => {
     requestData["sku"],
     requestData["quantity"]
   );
-  const batchref = model.allocate(line, batches);
+  if (!isValidSku(line.sku, batches)) {
+    return await {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: `Invalid sku ${line.sku}`,
+      }),
+    };
+  }
+
+  let batchref: string;
+  try {
+    batchref = model.allocate(line, batches);
+  } catch (e) {
+    if (e instanceof model.OutOfStockError) {
+      return await {
+        statusCode: 400,
+        body: JSON.stringify({
+          message: e.message,
+        }),
+      };
+    } else throw e;
+  }
+
+  const batch = batches.find((b) => b.reference === batchref) as model.Batch;
+  await repository.add(batch);
 
   const response = {
     statusCode: 201,
--- test/api.test.ts
+++ test/api.test.ts
@@ -59,4 +59,47 @@ describe("Allocations API", () => {
     expect(response.status).toBe(201);
     expect(response.data["batchref"]).toBe(earlyBatch);
   });
+
+  it("persists allocations", async () => {
+    const sku = uuid.v4();
+    const [batch1, batch2] = [uuid.v4(), uuid.v4()];
+    const [order1, order2] = [uuid.v4(), uuid.v4()];
+    await addStock(docClient)([
+      [batch1, sku, 10, "2011-01-01"],
+      [batch2, sku, 10, "2011-01-02"],
+    ]);
+    const line1 = { orderId: order1, sku, quantity: 10 };
+    const line2 = { orderId: order2, sku, quantity: 10 };
+
+    // first order uses up all stock in batch1
+    const r1 = await axios.post(`${config.getApiUrl()}/allocate`, line1);
+    expect(r1.status).toBe(201);
+    expect(r1.data["batchref"]).toBe(batch1);
+
+    // second order should go to batch 2
+    const r2 = await axios.post(`${config.getApiUrl()}/allocate`, line2);
+    expect(r2.status).toBe(201);
+    expect(r2.data["batchref"]).toBe(batch2);
+  });
+
+  it("returns a 400 message if the sku is out of stock", async () => {
+    const [sku, smallBatch, largeOrder] = [uuid.v4(), uuid.v4(), uuid.v4()];
+    await addStock(docClient)([[smallBatch, sku, 10, "2011-01-01"]]);
+    const data = { orderId: largeOrder, sku, quantity: 20 };
+    const r = await axios.post(`${config.getApiUrl()}/allocate`, data, {
+      validateStatus: null,
+    });
+    expect(r.status).toBe(400);
+    expect(r.data["message"]).toBe(`Out of stock for sku ${sku}`);
+  });
+
+  it("returns a 400 message if the sku is invalid", async () => {
+    const [unknownSku, orderId] = [uuid.v4(), uuid.v4()];
+    const data = { orderId, sku: unknownSku, quantity: 20 };
+    const r = await axios.post(`${config.getApiUrl()}/allocate`, data, {
+      validateStatus: null,
+    });
+    expect(r.status).toBe(400);
+    expect(r.data["message"]).toBe(`Invalid sku ${unknownSku}`);
+  });
 });

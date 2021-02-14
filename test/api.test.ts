76bd7881c6494e101d6e4c10b6017912d5ffdcb0
Brian Azizi <briana@theodo.co.uk>
2021-02-14 13:53:47 +0000

    Remove redundant e2e test

--- test/api.test.ts
+++ test/api.test.ts
@@ -37,7 +37,7 @@ describe("Allocations API", () => {
     done();
   });
 
-  it("returns allocations", async () => {
+  test("happy path returns a 201 and the allocated batch reference", async () => {
     const [sku, otherSku] = [uuid.v4(), uuid.v4()];
     const [earlyBatch, laterBatch, otherBatch] = [
       uuid.v4(),
@@ -51,38 +51,15 @@ describe("Allocations API", () => {
     ]);
 
     const requestData = { orderId: uuid.v4(), sku, quantity: 3 };
-    const url = config.getApiUrl();
     const response = await axios.post(
-      `${url}/allocate`,
+      `${config.getApiUrl()}/allocate`,
       JSON.stringify(requestData)
     );
     expect(response.status).toBe(201);
     expect(response.data["batchref"]).toBe(earlyBatch);
   });
 
-  it("persists allocations", async () => {
-    const sku = uuid.v4();
-    const [batch1, batch2] = [uuid.v4(), uuid.v4()];
-    const [order1, order2] = [uuid.v4(), uuid.v4()];
-    await addStock(docClient)([
-      [batch1, sku, 10, "2011-01-01"],
-      [batch2, sku, 10, "2011-01-02"],
-    ]);
-    const line1 = { orderId: order1, sku, quantity: 10 };
-    const line2 = { orderId: order2, sku, quantity: 10 };
-
-    // first order uses up all stock in batch1
-    const r1 = await axios.post(`${config.getApiUrl()}/allocate`, line1);
-    expect(r1.status).toBe(201);
-    expect(r1.data["batchref"]).toBe(batch1);
-
-    // second order should go to batch 2
-    const r2 = await axios.post(`${config.getApiUrl()}/allocate`, line2);
-    expect(r2.status).toBe(201);
-    expect(r2.data["batchref"]).toBe(batch2);
-  });
-
-  it("returns a 400 message if the sku is out of stock", async () => {
+  test("unhappy path returns a 400 and the error message", async () => {
     const [sku, smallBatch, largeOrder] = [uuid.v4(), uuid.v4(), uuid.v4()];
     await addStock(docClient)([[smallBatch, sku, 10, "2011-01-01"]]);
     const data = { orderId: largeOrder, sku, quantity: 20 };
@@ -91,15 +68,5 @@ describe("Allocations API", () => {
     });
     expect(r.status).toBe(400);
     expect(r.data["message"]).toBe(`Out of stock for sku ${sku}`);
-  });
-
-  it("returns a 400 message if the sku is invalid", async () => {
-    const [unknownSku, orderId] = [uuid.v4(), uuid.v4()];
-    const data = { orderId, sku: unknownSku, quantity: 20 };
-    const r = await axios.post(`${config.getApiUrl()}/allocate`, data, {
-      validateStatus: null,
-    });
-    expect(r.status).toBe(400);
-    expect(r.data["message"]).toBe(`Invalid sku ${unknownSku}`);
   });
 });

970cb946e260b01675bd64fb38b17b1455b76331
Brian Azizi <briana@theodo.co.uk>
2021-03-04 23:14:17 +0000

    Delete old table

--- startDynamo.sh
+++ startDynamo.sh
@@ -2,34 +2,6 @@ set -e
 
 docker-compose up -d
 sleep 2
-aws dynamodb create-table \
-    --table-name Allocations \
-    --attribute-definitions '[
-      {
-          "AttributeName": "PK",
-          "AttributeType": "S"
-      },
-      {
-          "AttributeName": "SK",
-          "AttributeType": "S"
-      }
-    ]' \
-    --key-schema '[
-      {
-          "AttributeName": "PK",
-          "KeyType": "HASH"
-      },
-      {
-          "AttributeName": "SK",
-          "KeyType": "RANGE"
-      }
-    ]' \
-    --provisioned-throughput '{
-      "ReadCapacityUnits": 1,
-      "WriteCapacityUnits": 1
-    }' \
-    --endpoint-url http://localhost:8000 \
-    --region eu-west-1
 
 aws dynamodb create-table \
     --table-name AllocationProducts  \
